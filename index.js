26f3eba3cb51c30c6ccf0c566781141bd7b78c66
Mark <markc.herring@gmail.com>
2021-05-04 10:29:25 +0900

    Added dotenv for MongoDB URI

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+// connect to mondgoDB database
+
+mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true});
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to database')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
