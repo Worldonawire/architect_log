734397e2a12712d5e7214b4f62b182e777dfc754
Mark <markc.herring@gmail.com>
2021-06-22 18:45:57 +0900

    added path details

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
 
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 const app = express();
 app.use(cors());
 
